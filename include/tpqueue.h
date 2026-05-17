80e99bf818e836ca31111acaf495eaf11bab67da
zephyr2007 <amir.karimov.0418@gmail.com>
2026-05-17 23:14:28 +0300

    aaaaaUpdate tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,62 +1,92 @@
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+#include <stdexcept>
+
+struct SYM {
+    char ch;
+    int prior;
+};
 
 template<typename T>
 class TPQueue {
  public:
-    TPQueue() :head(nullptr) {}
+    TPQueue() : head_(nullptr), count_(0) {}
+
     ~TPQueue() {
-        while (head) {
-            Node* next = head->next;
-            delete head;
-            head = next;
+        while (head_ != nullptr) {
+            Node* temp = head_;
+            head_ = head_->next;
+            delete temp;
         }
+    }
+
+    void push(const T& item) {
+        Node* new_node = new Node(item);
+
+        if (isEmpty()) {
+            head_ = new_node;
+        } else if (item.prior > head_->data.prior) {
+            new_node->next = head_;
+            head_ = new_node;
+        } else {
+            Node* current = head_;
+            // Ищем место для вставки
+            while (current->next != nullptr &&
+                   current->next->data.prior >= item.prior) {
+                current = current->next;
+            }
+            new_node->next = current->next;
+            current->next = new_node;
+        }
+        count_++;
     }
 
     T pop() {
-        if (head) {
-            T temp = head->data;
-            Node* next = head->next;
-            delete head;
-            head = next;
-            return temp;
-        } else {
-            return T();
+        if (isEmpty()) {
+            throw std::runtime_error("Queue is empty");
         }
+        Node* temp = head_;
+        T result = head_->data;
+        head_ = head_->next;
+        delete temp;
+        count_--;
+        return result;
     }
 
-    void push(T data) {
-        if (head) {
-            Node* temp = head;
-            if (head->data.prior < data.prior) {
-                head = new Node;
-                head->data = data;
-                head->next = nullptr;
-                head->next = temp;
-            } else {
-                while ((temp->next != nullptr)
-                    && data.prior <= temp->next->data.prior)
-                    temp = temp->next;
-                Node* newNode = new Node;
-                newNode->data = data;
-                newNode->next = nullptr;
-                newNode->next = temp->next;
-                temp->next = newNode;
-            }
-        } else {
-            head = new Node;
-            head->data = data;
-            head->next = nullptr;
+    T front() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Queue is empty");
         }
+        return head_->data;
+    }
+
+    bool isEmpty() const {
+        return head_ == nullptr;
+    }
+
+    int size() const {
+        return count_;
+    }
+
+    void clear() {
+        while (head_ != nullptr) {
+            Node* temp = head_;
+            head_ = head_->next;
+            delete temp;
+        }
+        count_ = 0;
     }
 
  private:
     struct Node {
         T data;
         Node* next;
+        explicit Node(const T& value) : data(value), next(nullptr) {}
     };
-    Node* head;
+
+    Node* head_;
+    int count_;
 };
-struct SYM {
-    char ch;
-    int prior;
-};
+
 #endif  // INCLUDE_TPQUEUE_H_
